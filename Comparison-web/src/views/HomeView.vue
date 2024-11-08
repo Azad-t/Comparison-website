22d98cc52fa67d62439495f53e4905543015355d
Zhujunhao <2050113341@qq.com>
2024-11-09 01:20:07 +0800

    vue init

---
+++ Comparison-web/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    TheWelcome,
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <HelloWorld />
+  </main>
+</template>
